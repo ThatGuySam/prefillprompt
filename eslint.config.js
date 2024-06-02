a6f71b85f08c89cd8e86510fa2333a417a82e525
ThatGuySam <sam@sam.lc>
2024-06-02 16:07:52 -0500

    Turn off refer-separate-static-class

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,14 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  vue: true,
-})
+export default antfu(
+  {
+    vue: true,
+  },
+  {
+    // Remember to specify the file glob here, otherwise it might cause the vue plugin to handle non-vue files
+    files: ['**/*.vue'],
+    rules: {
+      'vue/prefer-separate-static-class': 'off',
+    },
+  },
+)
