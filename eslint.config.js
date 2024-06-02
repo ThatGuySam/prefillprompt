5012b2576124aa6f49d947892684bd798391a356
ThatGuySam <sam@sam.lc>
2024-06-02 16:34:31 -0500

    Set indenting to 4 spaces

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,19 @@
 import antfu from '@antfu/eslint-config'
 
+const DEFAULT_SPACE_INDENT = 4
+
 export default antfu(
-  {
-    vue: true,
-  },
-  {
-    // Remember to specify the file glob here, otherwise it might cause the vue plugin to handle non-vue files
-    files: ['**/*.vue'],
-    rules: {
-      'vue/prefer-separate-static-class': 'off',
+    {
+        vue: true,
+        stylistic: {
+            indent: DEFAULT_SPACE_INDENT,
+        },
     },
-  },
+    {
+        // Remember to specify the file glob here, otherwise it might cause the vue plugin to handle non-vue files
+        files: ['**/*.vue'],
+        rules: {
+            'vue/prefer-separate-static-class': 'off',
+        },
+    },
 )
