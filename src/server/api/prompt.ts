7cd07c72f576c5aaf21419eb8c0970db55529b63
ThatGuySam <sam@sam.lc>
2024-06-02 18:05:09 -0500

    Redirect to ChatGPT

--- src/server/api/prompt.ts
+++ src/server/api/prompt.ts
@@ -1,5 +1,8 @@
 import assert from 'node:assert'
-export default defineEventHandler((event) => {
+
+const UNCACHED_REDIRECT_CODE = 307
+
+export default defineEventHandler(async (event) => {
     const { q: promptText } = getQuery(event)
 
     assert( typeof promptText === 'string', 'Expected prompt text to be a string' )
@@ -7,9 +10,9 @@ export default defineEventHandler((event) => {
 
     console.log(`Received query: `, promptText )
   
-    return {
-        body: {
-            message: `Hello, ${promptText}!`
-        }
-    }
+    await sendRedirect(
+        event, 
+        `https://chat.openai.com?q=${ encodeURIComponent(promptText) }`,
+        UNCACHED_REDIRECT_CODE
+    )
 })
