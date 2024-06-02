4875a7f6854d11adb0f93b922a9dbe1c0ae138f8
ThatGuySam <sam@sam.lc>
2024-06-02 17:54:17 -0500

    Parse q param

--- src/server/api/prompt.ts
+++ src/server/api/prompt.ts
@@ -1,5 +1,15 @@
+import assert from 'node:assert'
 export default defineEventHandler((event) => {
-    const name = getRouterParam(event, 'name')
+    const { q: promptText } = getQuery(event)
+
+    assert( typeof promptText === 'string', 'Expected prompt text to be a string' )
+    assert( promptText.length > 0, 'Expected prompt text to be non-empty' )
+
+    console.log(`Received query: `, promptText )
   
-    return `Hello, ${name}!`
+    return {
+        body: {
+            message: `Hello, ${promptText}!`
+        }
+    }
 })
