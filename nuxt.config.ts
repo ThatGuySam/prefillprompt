eb1e06904a7e27324de644e033e5b944e8113d06
ThatGuySam <sam@sam.lc>
2024-06-02 22:06:07 -0500

    Turn off ssr for index

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,8 @@ export default defineNuxtConfig({
         'nuxt-icon',
         '@vueuse/nuxt',
     ],
+    routeRules: {
+        // Homepage pre-rendered at build time
+        '/': { ssr: false },
+    },
 })
