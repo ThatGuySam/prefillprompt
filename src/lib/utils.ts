9003c6de80eaa831dcad4b95314fb8589bd5b4b9
ThatGuySam <sam@sam.lc>
2024-06-02 20:19:53 -0500

    Add getSiteHost util

---
+++ src/lib/utils.ts
@@ -0,0 +1,9 @@
+export function getSiteHost() {
+    const hasWindow = typeof window !== 'undefined'
+
+    if (!hasWindow && window.location.href.length > 2) {
+        return window.location.href
+    }
+
+    return 'http://localhost:3000'
+}
