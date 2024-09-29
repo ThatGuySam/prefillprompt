25a1fe7671f179aa817da7999947da3598166751
ThatGuySam <sam@sam.lc>
2024-09-29 11:55:20 -0400

    add: isModel type guard

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,19 @@
 import { hasWindow } from 'std-env'
 
+import type { LLMModel } from '~/lib/types'
+
 export function getSiteHost() {
     if (!hasWindow && window?.location?.href?.length > 2) {
         return window.location.host
     }
 
     return undefined
+}
+
+export function isModel(modelName: unknown): modelName is LLMModel {
+    if (typeof modelName !== 'string') {
+        return false
+    }
+
+    return ['chatgpt', 'claude', 'perplexity'].includes(modelName)
 }
