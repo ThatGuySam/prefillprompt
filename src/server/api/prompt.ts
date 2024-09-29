7e25726a25e8b8caa24eaf1950b2e837e2a293a5
ThatGuySam <sam@sam.lc>
2024-09-29 12:38:41 -0400

    update: enable specifying modelName on links

--- src/server/api/prompt.ts
+++ src/server/api/prompt.ts
@@ -1,18 +1,30 @@
 import assert from 'node:assert'
+import { isModel } from '~/lib/utils'
 
 const UNCACHED_REDIRECT_CODE = 307
 
 export default defineEventHandler(async (event) => {
-    const { q: promptText } = getQuery(event)
+    const {
+        q: promptText,
+        m: modelName = 'chatgpt',
+    } = getQuery(event)
 
-    assert( typeof promptText === 'string', 'Expected prompt text to be a string' )
-    assert( promptText.length > 0, 'Expected prompt text to be non-empty' )
+    assert(typeof promptText === 'string', 'Expected prompt text to be a string')
+    assert(promptText.length > 0, 'Expected prompt text to be non-empty')
 
-    console.log(`Received query: `, promptText )
-  
-    await sendRedirect(
-        event, 
-        `https://chat.openai.com?q=${ encodeURIComponent(promptText) }`,
-        UNCACHED_REDIRECT_CODE
-    )
+    // Assert the model name is valid
+    assert(isModel(modelName), `Invalid model name: ${modelName}`)
+
+    console.log(`Received query: `, promptText)
+
+    if (modelName === 'chatgpt') {
+        await sendRedirect(
+            event,
+            `https://chat.openai.com?q=${encodeURIComponent(promptText)}`,
+            UNCACHED_REDIRECT_CODE,
+        )
+        return
+    }
+
+    throw new Error(`Model ${modelName} is not yet supported`)
 })
