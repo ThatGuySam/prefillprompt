448a6769f895bc963ba78ed4ba663a4e2715513a
ThatGuySam <sam@sam.lc>
2024-09-30 16:47:00 -0400

    update: enable testing prompt from send button

--- src/components/ChatUI.vue
+++ src/components/ChatUI.vue
@@ -276,19 +276,26 @@ onMounted(() => {
                                             placeholder=""
                                         />
 
-                                        <span class="inline-flex h-full items-end font-light text-gray-500">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                class="h-8 w-8"
-                                                viewBox="0 0 20 20"
-                                                fill="currentColor"
-                                            >
-                                                <path
-                                                    fill-rule="evenodd"
-                                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z"
-                                                    clip-rule="evenodd"
-                                                />
-                                            </svg>
+                                        <span
+                                            :class="[
+                                                'inline-flex h-full items-end font-light',
+                                                hasAnyInput ? '' : 'text-gray-500',
+                                            ]"
+                                        >
+                                            <a :href="promptUrl" target="_blank">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    class="h-8 w-8"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
+                                                >
+                                                    <path
+                                                        fill-rule="evenodd"
+                                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z"
+                                                        clip-rule="evenodd"
+                                                    />
+                                                </svg>
+                                            </a>
                                         </span>
                                     </div>
                                 </div>
