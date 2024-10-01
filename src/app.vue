9c69f6a5184e3627e6e90093614b279340d40849
ThatGuySam <sam@sam.lc>
2024-09-30 20:53:19 -0400

    add: github link

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,16 @@
 <template>
-    <div class="dark:bg-gray-900 min-h-screen flex justify-center items-center">
+    <div class="dark:bg-gray-900 min-h-screen flex flex-col justify-center items-center">
+        <nav class="absolute inset-x-0 top-0 w-full h-16 flex justify-end items-center px-4">
+            <!-- End Nav -->
+            <div class="flex items-center space-x-4">
+                <!-- GitHub Repo -->
+                <a
+                    href="https://github.com/ThatGuySam/prefillprompt"
+                    target="_blank"
+                    class="hover:text-gray-300 text-gray-400"
+                >GitHub</a>
+            </div>
+        </nav>
         <ChatUI />
     </div>
     <UNotifications />
