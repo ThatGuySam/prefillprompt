19dd4a0435ac9ff0806d3479a1ebd2b1d6066721
ThatGuySam <sam@sam.lc>
2024-09-30 20:42:48 -0400

    update: set nuxt compatibilityDate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    compatibilityDate: '2024-09-30',
     devtools: { enabled: false },
     srcDir: 'src/',
     modules: [
